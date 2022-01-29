9d08c9f0c8e043c32f415fa3962a0978a7110283
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-01-29 11:57:39 +0700

    Add project files.

---
+++ SK-Lib-cstdio-rename-Bagian5__CPP/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <conio.h>
+
+/*
+    Source by CPPReference
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+int main() {
+    bool ok{ std::ofstream("from.txt").put('a') }; // create and write to file
+
+    if (!ok) {
+        std::perror("Error creating from.txt");
+        return 1;
+    }
+
+    if (std::rename("from.txt", "to.txt")) {
+        std::perror("Error renaming");
+        return 1;
+    }
+
+    std::cout << std::ifstream("to.txt").rdbuf() << '\n'; // print file
+
+    _getch();
+    return 0;
+}
